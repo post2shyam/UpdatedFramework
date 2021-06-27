299e8e67b42760a3d5aebd312d02f1ff0835d1ac
Shyam Grover <post2shyam@gmail.com>
2021-06-27 21:22:27 +0530

    In progress

---
+++ app/src/main/java/com/post2shyam/updatedframework/application/di/ApplicationModule.kt
@@ -0,0 +1,10 @@
+package com.post2shyam.updatedframework.application.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+}
