0b2a377df9b4dd9c6e0a7a29e84e52a201668cf6
Shyam Grover <post2shyam@gmail.com>
2021-06-29 18:53:04 +0530

    Persistance store exposed under application / system level component.

--- app/src/main/java/com/post2shyam/updatedframework/application/di/ApplicationModule.kt
+++ app/src/main/java/com/post2shyam/updatedframework/application/di/ApplicationModule.kt
@@ -1,10 +1,20 @@
 package com.post2shyam.updatedframework.application.di
 
+import android.content.Context
+import com.post2shyam.updatedframework.application.PersistanceStore
+import com.post2shyam.updatedframework.application.storage.IPersistanceStore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ApplicationModule {
+
+    @Provides
+    fun providesPersistanceStore(@ApplicationContext context: Context) : IPersistanceStore{
+        return PersistanceStore(context)
+    }
 }
