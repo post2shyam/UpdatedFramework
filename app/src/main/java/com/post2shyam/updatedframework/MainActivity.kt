8b6d7f2a8f228d16cab09e7b0651410cb715a74e
Shyam Grover <post2shyam@gmail.com>
2021-06-30 18:02:17 +0530

    Successfully injected Persistance store exposed under application / system level component to activity.

--- app/src/main/java/com/post2shyam/updatedframework/MainActivity.kt
+++ app/src/main/java/com/post2shyam/updatedframework/MainActivity.kt
@@ -2,10 +2,20 @@ package com.post2shyam.updatedframework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.post2shyam.updatedframework.application.PersistanceStore
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var persistanceStore: PersistanceStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        persistanceStore.writeInt(90)
     }
 }
--- app/src/main/java/com/post2shyam/updatedframework/application/PersistanceStore.kt
+++ app/src/main/java/com/post2shyam/updatedframework/application/PersistanceStore.kt
@@ -4,17 +4,20 @@ import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import com.post2shyam.updatedframework.application.storage.IPersistanceStore
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
 
 class PersistanceStore : IPersistanceStore {
 
     private var sharedPreferences: SharedPreferences?
 
-    constructor(context: Context) {
+    @Inject
+    constructor(@ApplicationContext context: Context) {
         sharedPreferences = context.getSharedPreferences("kk", MODE_PRIVATE)
     }
 
     override fun readInt(): Int {
-        //If the value is not found return default 0. If the sharedpreference not found then also return 0
+        //If the value is not found return default 0. If the shared preference not found then also return 0
         return sharedPreferences?.getInt("FF", 0) ?: 0;
     }
 
